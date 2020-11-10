544bc2faabba20a019c646587dfb7f8e90cc7c10
lucasrviana <lucasrviana@gmail.com>
2020-11-10 09:53:14 -0300

    add Api

---
+++ Web2Api/Web2Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Web2Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Obtervalores")]
+        public IEnumerable<WeatherForecast> Obtervalores()
+        {
+            var rng = new Random();
+
+            var valores = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            if (valores.Length <= 50000)
+                return null;
+
+            return valores;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<WeatherForecast>> ObterTodos()
+        {
+            var rng = new Random();
+            var valor =  Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            return Ok(valor);
+        }
+    }
+}
--- Web2Api/Web2Api/obj/Debug/netcoreapp3.1/Web2Api.AssemblyInfo.cs
+++ Web2Api/Web2Api/obj/Debug/netcoreapp3.1/Web2Api.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("meu1api")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Web2Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("meu1api")]
-[assembly: System.Reflection.AssemblyTitleAttribute("meu1api")]
+[assembly: System.Reflection.AssemblyProductAttribute("Web2Api")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Web2Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
