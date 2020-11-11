7ae61cc55791a33c98265aef58ff7eb27fbaa5aa
lucasrviana <lucasrviana@gmail.com>
2020-11-11 10:28:51 -0300

    aprendizado

--- Web2Api/Web2Api/Controllers/WeatherForecastController.cs
+++ Web2Api/Web2Api/Controllers/WeatherForecastController.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace Web2Api.Controllers
 {
-    [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class WeatherForecastController : MainController
     {
         private static readonly string[] Summaries = new[]
         {
@@ -43,6 +43,25 @@ namespace Web2Api.Controllers
         }
 
         [HttpGet]
+        public ActionResult ObterResultado()
+        {
+            var rng = new Random();
+
+            var valores = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            if (valores.Length <= 50000)
+                return BadRequest();
+
+            return Ok();
+        }
+
+        [HttpGet]
         public ActionResult<IEnumerable<WeatherForecast>> ObterTodos()
         {
             var rng = new Random();
@@ -56,5 +75,34 @@ namespace Web2Api.Controllers
 
             return Ok(valor);
         }
+
+
+        [HttpPost]
+        [ProducesResponseType(typeof(WeatherForecast), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult ObterTodos(WeatherForecast value)
+        {
+            var rng = new Random();
+            var valor = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToList();
+
+            valor.Add(value);
+
+
+            if (!valor.Contains(value))
+                return BadRequest();
+
+            return CreatedAtAction("Post",valor);
+        }
+    }
+
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        
     }
 }
